ea98cec1674df482047e5f90b114e7cf307ba0b7
German <14171224@itculiacan.edu.mx>
2018-08-12 10:33:44 -0600

    Mostrar detalles en cada platillo
    mostrando la cantidad y la medida de cada ingrediente

--- app/serializers/dish_serializer.rb
+++ app/serializers/dish_serializer.rb
@@ -1,9 +1,18 @@
 class DishSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :recipe, :image, :users_ids
-  has_many :ingredients
+  has_many :dish_ingredients
 
 
     def users_ids
       object.users.size
+    end
+    def dish_ingredients
+      customized_dish_ingredients = []
+      object.dish_ingredients.each do |dish_ingredient|
+        custom_dish_ingredient = dish_ingredient.slice(:id, :measure, :quantity)
+        custom_dish_ingredient[:ingredient] = dish_ingredient.ingredient.slice(:id, :name, :image)
+        customized_dish_ingredients.push(custom_dish_ingredient)
+      end
+      return customized_dish_ingredients
     end
 end
