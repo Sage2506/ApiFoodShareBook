7406fe08dea3dd30c00ba00f2b41136ab3de215f
Eduardo Salazar <german2506.gs@gmail.com>
2021-05-09 15:04:51 -0600

    permission list correction in roles serializer

--- app/serializers/role_serializer.rb
+++ app/serializers/role_serializer.rb
@@ -4,9 +4,10 @@ class RoleSerializer < ActiveModel::Serializer
   def permissions
     self.object.permissions.map do | permission |
       {
+        name: permission.name,
+        description: permission.description,
         type_id: permission.permission_type.id,
-        type_name: permission.permission_type.name,
-        list: permission.list
+        type_name: permission.permission_type.name
       }
     end
   end
