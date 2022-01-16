7f775e391a82b5af14433e9f20adf030da07a6e3
Eduardo Salazar <german2506.gs@gmail.com>
2022-01-16 14:32:49 -0700

    added user_id to ingredient serializer

--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,10 +1,10 @@
 class IngredientSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image, :measures
+  attributes :id, :name, :description, :image, :measures, user_id
 
   def measures
     self.object.measures.map do |measure|
       measure.id
     end
   end
-  
+
 end
