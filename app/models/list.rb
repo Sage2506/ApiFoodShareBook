0576b1ce5ab95f7f73ef60172e71bc7dffcd5fd3
Eduardo Salazar <german2506.gs@gmail.com>
2022-03-08 20:13:43 -0700

    added dependent to list items
    list items will be destroyed when list is deleted

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 class List < ApplicationRecord
   belongs_to :user
-  has_many :list_items
+  has_many :list_items, dependent: :destroy
   validates :name, presence: true
 
-  def generate_list_items(meals)
-
-  end
+  def generate_list_items(meals); end
 end
