1e68a4e7ef90ad57fc2597b1e731000fdf4425c2
Eduardo Salazar <german2506.gs@gmail.com>
2022-05-29 13:44:08 -0600

    new measure endpoint

--- app/controllers/api/v1/measures_controller.rb
+++ app/controllers/api/v1/measures_controller.rb
@@ -3,6 +3,7 @@
 module Api
   module V1
     class MeasuresController < ApplicationController
+      before_action :authenticate_request!, only: %i[create update destroy]
       before_action :set_measure, only: %i[show update]
       def index
         paginate json: Measure.ransack(params[:q]).result
@@ -10,6 +11,15 @@ module Api
 
       def show
         render json: @measure
+      end
+
+      def create
+        measure = Measure.new(measure_params)
+        if measure.save
+          render json: measure
+        else
+          render status: :internal_server_error, json: { message: measure.errors.full_message}
+        end
       end
 
       def update
